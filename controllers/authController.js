970f4f4efe2c266b9a51cf77ddf4a6985afb59c9
Florian9543 <imbaud.florian@orange.fr>
2024-03-25 09:31:36 +0100

    Résolution de dépendances cassées

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,17 @@ const config = require('../config/config');
 
 exports.register = async (req, res) => {
   // Implémenter la logique d'inscription
+  const { email, password } = req.body;
+  const user = { email, password };
+  const token = jwt.sign(user, config.secret);
+  res.status(201).json({ token });
+
 };
 
 exports.login = async (req, res) => {
   // Implémenter la logique de connexion
+  const { email, password } = req.body;
+  const user = { email, password };
+  const token = jwt.sign(user, config.secret);
+  res.status(200).json({ token });
 };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,4 +4,25 @@ const config = require('../config/config');
 
 module.exports = (req, res, next) => {
   // Implémenter le middleware d'authentification JWT
+  // Récupérer le token JWT depuis le header de la requête
+
+  // Vérifier si le token est valide
+
+  // Si le token est valide, décoder le token et ajouter l'objet user à la requête
+
+  // Si le token n'est pas valide, retourner une erreur 401 Unauthorized
+
+  // Passer à l'exécution du middleware suivant
+
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.user = decoded;
+    next();
+  });
 };
